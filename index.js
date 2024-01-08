f24458475c5fd602450750ee1965f61f913a16a5
ANAINAP <anainarajeev3@gmail.com>
2024-01-08 15:10:14 +0530

    ch10

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World! good after noon'); //write a response to the client
+  res.write('Hello World! good after-noon'); //write a response to the client
   res.end(); //end the response
 }).listen(4000,()=>{
     console.log("server running on 4000")
