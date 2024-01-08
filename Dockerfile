8b141d7cf73a4a5279bf3a6939526984482f9ac8
ANAINAP <anainarajeev3@gmail.com>
2024-01-08 12:25:12 +0530

    ch3

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /AxoSDKDocs
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 3000
+EXPOSE 4000
 CMD ["npm","start"]
--- dockerpull3.sh
+++ dockerpull3.sh
@@ -13,4 +13,4 @@ sudo docker pull -a "$DOCKER_USERNAME/$IMAGE_NAME"
 # Stop and remove any existing container with the same name
 sudo docker rm -f "$CONTAINER_NAME" 2> /dev/null || true
 # Run the container, exposing ports (but without volumes)
-sudo docker run -dp 8080:8080 --name "$CONTAINER_NAME" "$DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
+sudo docker run -dp 4000:4000 --name "$CONTAINER_NAME" "$DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
